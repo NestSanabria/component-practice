50f19a89c76f7bd5f22a1feab1bc2ee75d7c9e39
Nest <155921944+Nest38@users.noreply.github.com>
2024-05-24 18:37:38 -0500

    complete exercise

--- src/components/ThirdComponent.jsx
+++ src/components/ThirdComponent.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const ThirdComponent = (props) => {
-
-    console.log(props)
+export const ThirdComponent = ({ name, lastName, card}) => {
 
   return (
     <div>
         <h2>Comunicación entre Componentes</h2>
         <ul>
-            <li>{ props.name }</li>
-            <li>{ props.lastName }</li>
-            <li>{ props.card.allergies }</li>
+            <li>{ name }</li>
+            <li>{ lastName }</li>
+            <li>{ card.height }</li>
+            <li>{ card.bloodGroup }</li>
+            <li>{ card.allergies }</li>
         </ul>
     </div>
   )
+}
+
+ThirdComponent.propTypes = {
+  name: PropTypes.string,
+  lastName: PropTypes.string,
+  card: PropTypes.object
 }
 
 // Hooks - eventos
